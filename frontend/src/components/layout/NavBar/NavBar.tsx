2a36cfbc84b84c98cf618b5c5183ea342b0272a4
jonar12 <jona.arredondo12@gmail.com>
2023-10-20 15:15:19 -0600

    updated submitContract function

--- frontend/src/components/layout/NavBar/NavBar.tsx
+++ frontend/src/components/layout/NavBar/NavBar.tsx
@@ -1,88 +1,88 @@
 import {useState} from "react"
-import {GearApi,GearKeyring} from "@gear-js/api";
+import {GearApi, GearKeyring} from "@gear-js/api";
 import CustomAlert from "../CustomAlert/CustomAlert";
 import './navBar.css'
 
-const submitContract = async function (content:string) {
-    const gearApi = await GearApi.create({
-        providerAddress: 'wss://vit.vara-network.io',
-    });
-    const keyring = await GearKeyring.fromSuri('//Alice');
+const submitContract = async function (content: string) {
+	const gearApi = await GearApi.create({
+		providerAddress: 'wss://vit.vara-network.io',
+	});
+	const keyring = await GearKeyring.fromSuri('//Alice');
 
-    // Replace the 'fs.readFileSync' call with browser-friendly file loading
-    // Example using fetch to load a file:
-    try {
-        const response = await fetch('./../../../../../smart_contracts/thread/target/wasm32-unknown-unknown/release/thread.wasm');
-        if (!response.ok) {
-            throw new Error("Failed to fetch the file.");
-        }
+	try {
+		const response = await fetch('./../../../../../smart_contracts/thread/target/wasm32-unknown-unknown/release/thread.wasm');
+		if (!response.ok) {
+			throw new Error("Failed to fetch the file.");
+		}
 
-        const arrayBuffer = await response.arrayBuffer();
-        const code = new Uint8Array(arrayBuffer);
-        const payload = {
-            "id": "1",
-            "owner": "100",
-            "postType": "Challenge",
-            "content": content,
-            "replies": "sdsfdsfdsf",
-            "state": "Active"
-        }
+		const arrayBuffer = await response.arrayBuffer();
+		const code = new Uint8Array(arrayBuffer);
+		const payload = {
+			"id": "1",
+			"owner": "100",
+			"postType": "Challenge",
+			"content": content,
+			"replies": "sdsfdsfdsf",
+			"state": "Active"
+		}
 
-        const program = {
-            code,
-            gasLimit: 1000000,
-            value: 1000000000000000,
-            initPayload: payload,
-        };
+		const program = {
+			code,
+			gasLimit: 1000000,
+			value: 1000000000000000,
+			initPayload: payload,
+		};
 
-        try {
-            const { programId, codeId, salt, extrinsic } = gearApi.program.upload(
-                program
-            );
-
-            try {
-                await extrinsic.signAndSend(keyring, (event: { toHuman: () => any; }) => {
-                    console.log(event.toHuman());
-                });
-            } catch (error) {
-                // @ts-ignore
-                console.error(`${error.name}: ${error.message}`);
-            }
-        } catch (error) {
-            // @ts-ignore
-            console.error(`${error.name}: ${error.message}`);
-        }
-    } catch (error) {
-        // Handle file loading error
-        // @ts-ignore
-        console.error(`${error.name}: ${error.message}`);
-    }
+		try {
+			const {programId, codeId, salt, extrinsic} = gearApi.program.upload(
+				program
+			);
+			try {
+				await extrinsic.signAndSend(keyring, (event: { toHuman: () => any; }) => {
+					console.log(event.toHuman());
+				});
+			} catch (error) {
+				// @ts-ignore
+				console.error(`${error.name}: ${error.message}`);
+			}
+		} catch (error) {
+			// @ts-ignore
+			console.error(`${error.name}: ${error.message}`);
+		}
+	} catch (error) {
+		// Handle file loading error
+		// @ts-ignore
+		console.error(`${error.name}: ${error.message}`);
+	}
 }
-function createPost(){
+
+function createPost() {
 
 }
 
 
 function NavBar() {
-    const [showAlert, setShowAlert] = useState(false);
+	const [showAlert, setShowAlert] = useState(false);
 
-    const handleShowAlert = () => {
-        setShowAlert(true);
-    };
+	const handleShowAlert = () => {
+		setShowAlert(true);
+	};
 
-    const handleConfirm = (input1: string, input2: string) => {
-        submitContract(input2)
-        console.log('Input 1:', input1);
-        console.log('Input 2:', input2);
-    };
-        return (<div className="navBar">
-            < button type='button' id="navBtn1" className="navBarButton"/>
-            < button type='button' id="navBtn2" className="navBarButton"/>
-            < button type='button' onClick={handleShowAlert} id="navBtn3" className="navBarButton"/>
-            <CustomAlert isOpen={showAlert} onClose={() => setShowAlert(false)} onConfirm={handleConfirm} />
-            < button type='button' id="navBtn4" className="navBarButton"/>
-            < button type='button' id="navBtn5" className="navBarButton"/>
-        </div>)
+	const handleConfirm = async (input1: string, input2: string) => {
+		await submitContract(input2)
+		console.log('Input 1:', input1);
+		console.log('Input 2:', input2);
+	};
+	return (
+		<div className="navBar">
+			<button type='button' id="navBtn1" className="navBarButton"/>
+			<button type='button' id="navBtn2" className="navBarButton"/>
+			<button type='button' onClick={handleShowAlert} id="navBtn3" className="navBarButton"/>
+			<CustomAlert isOpen={showAlert} onClose={() => setShowAlert(false)} onConfirm={handleConfirm}/>
+			<button type='button' id="navBtn4" className="navBarButton"/>
+			<button type='button' id="navBtn5" className="navBarButton"/>
+		</div>
+	)
 
 }
 
