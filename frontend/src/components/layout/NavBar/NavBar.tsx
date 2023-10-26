1f6915f4ef9891c5c678a180dd3a63380611191e
jonar12 <jona.arredondo12@gmail.com>
2023-10-25 22:46:48 -0600

    removed unused handleConfirm

--- frontend/src/components/layout/NavBar/NavBar.tsx
+++ frontend/src/components/layout/NavBar/NavBar.tsx
@@ -11,17 +11,12 @@ function NavBar() {
 		setShowAlert(true);
 	};
 
-	const handleConfirm = async (input1: string, input2: string) => {
-		console.log('Input 1:', input1);
-		console.log('Input 2:', input2);
-	};
-
 	return (
 		<div className="navBar">
 			<button type='button' id="navBtn1" className="navBarButton"/>
 			<button type='button' id="navBtn2" className="navBarButton"/>
 			<button type='button' onClick={handleShowAlert} id="navBtn3" className="navBarButton"/>
-			<CustomAlert isOpen={showAlert} onClose={() => setShowAlert(false)} onConfirm={handleConfirm}/>
+			<CustomAlert type={0} isOpen={showAlert} onClose={() => setShowAlert(false)}/>
 			<button type='button' id="navBtn4" className="navBarButton"/>
 			<button type='button' id="navBtn5" className="navBarButton"/>
 		</div>
