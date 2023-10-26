b5feef6f275d66d6c7307048e592c7cde776efb2
jonar12 <jona.arredondo12@gmail.com>
2023-10-26 03:06:49 -0600

    added function to find winner of the thread

--- smart_contracts/thread/src/lib.rs
+++ smart_contracts/thread/src/lib.rs
@@ -22,11 +22,11 @@ struct Thread {
 
 impl Thread {
     async fn add_liquidity( &mut self, amount_tokens: u128) {
-        let currentstate = thread_state_mut();
+        let current_state = thread_state_mut();
         let address_ft = addresft_state_mut();
         let payload = FTAction::Burn(amount_tokens);
         let result =  msg::send_for_reply_as::<_, FTEvent>(address_ft.ft_program_id,payload,0,0).expect("Error in sending a message").await;
-        currentstate.participants.entry(msg::source()).or_insert(amount_tokens); 
+        current_state.participants.entry(msg::source()).or_insert(amount_tokens); 
 
         let _ = match result {
             Ok(event) => match event {
@@ -53,20 +53,37 @@ impl Thread {
         };
     }
 
-    async fn tokens_transfer_reward(&mut self, amount_tokens: u128) {
-        let currentstate = thread_state_mut();
+    async fn tokens_transfer_reward(&mut self, amount_tokens: u128, dest: ActorId) {
+        let current_state = thread_state_mut();
         let address_ft = addresft_state_mut();           
-        let payload = FTAction::Transfer{from: exec::program_id(), to: msg::source() ,amount: amount_tokens};
+        let payload = FTAction::Transfer{from: exec::program_id(), to: dest ,amount: amount_tokens};
         let _ = msg::send(address_ft.ft_program_id, payload, 0);
-        currentstate.participants.entry(msg::source()).or_insert(amount_tokens);
     }
 
     async fn tokens_transfer_pay(&mut self, amount_tokens: u128) {
-        let currentstate = thread_state_mut();
+        let current_state = thread_state_mut();
         let address_ft = addresft_state_mut();
         let payload = FTAction::Transfer{from: msg::source() , to: exec::program_id(), amount: amount_tokens};
         let _ = msg::send(address_ft.ft_program_id, payload, 0);
-        currentstate.participants.entry(msg::source()).or_insert(amount_tokens);
+        current_state.participants.entry(msg::source()).or_insert(amount_tokens);
+        current_state.distributed_tokens += amount_tokens;
+    }
+
+    fn find_reply_with_most_likes(&mut self) -> Option<&ActorId> {
+        let current_state = thread_state_mut();
+        // Initialize variables to keep track of the reply with the most likes
+        let mut max_likes = 0;
+        let mut actor_id_with_most_likes: Option<&ActorId> = None;
+
+        // Iterate through the replies in the HashMap
+        for (actor_id, reply) in &current_state.replies {
+            if reply.number_of_likes > max_likes {
+                max_likes = reply.number_of_likes;
+                actor_id_with_most_likes = Some(actor_id);
+            }
+        }
+
+        actor_id_with_most_likes
     }
 }
 
@@ -123,6 +140,8 @@ async fn main() {
             new_thread.title = init_thread.title;
             new_thread.content = init_thread.content;
             new_thread.state = ThreadState::Active;
+
+            new_thread.tokens_transfer_pay(1).await;
         }
 
         ThreadAction::AddReply(reply) => {
@@ -150,7 +169,9 @@ async fn main() {
 
         ThreadAction::EndThread => {
             let thread = thread_state_mut();
-            thread.state = ThreadState::Expired ;
+            thread.state = ThreadState::Expired;
+            let &winner = thread.find_reply_with_most_likes().expect("Winner not found");
+            thread.tokens_transfer_reward(1, winner).await;
         }
     };
 }
