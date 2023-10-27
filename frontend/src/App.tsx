a714cd494c2f2b7e48f7f630518d6be21b29514e
Nivek <kevin500nunez@gmail.com>
2023-10-27 16:02:04 -0600

    Array de rectangulos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { Header, ApiLoader } from 'components';
 import { withProviders } from 'hocs';
 import './normalize.css';
 import Post from "./components/layout/post/post";
+import PostCard from "./components/layout/PostCard/PostCard";
+import {useState} from "react";
 
 function Component() {
   const { isApiReady } = useApi();
@@ -11,13 +13,32 @@ function Component() {
 
   const isAppReady = isApiReady && isAccountReady;
 
+  const [showRectangles, setShowRectangles] = useState(false);
+  const numberOfRectangles = 5; // Número de rectángulos a mostrar
+    const generateRectangles = () => {
+        setShowRectangles(true);
+    };
+
+    const rectangles = Array.from({ length: numberOfRectangles }, (_, index) => (
+        <div key={index} className="blue-rectangle"></div>
+    ));
+
   return (
     <>
       <Header isAccountVisible={isAccountReady} />
       <main>{isAppReady ? <Routing /> : <ApiLoader />}</main>
         <div>
-            <h1>AAAAAAAAAA</h1>
             <Post />
+        </div>
+        <PostCard title={'OLA'} content={'xdxdxdxd'} type={1}></PostCard>
+        <div>
+            <h1>Generador de Rectángulos Azules</h1>
+            <button onClick={generateRectangles}>Mostrar Rectángulos</button>
+            {showRectangles && (
+                <div className="rectangle-container">
+                    {rectangles}
+                </div>
+            )}
         </div>
     </>
   );
