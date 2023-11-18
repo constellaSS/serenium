31780ca824c5dd53a94cb7d53d60747e871265a7
jonar12 <jona.arredondo12@gmail.com>
2023-11-18 16:41:15 -0600

    Removed journal and chat buttons.

--- frontend/src/components/layout/NavBar/NavBar.tsx
+++ frontend/src/components/layout/NavBar/NavBar.tsx
@@ -14,14 +14,11 @@ function NavBar() {
 	return (
 		<div className="navBar">
 			<button type='button' id="navBtn1" className="navBarButton"/>
-			<button type='button' id="navBtn2" className="navBarButton"/>
 			<button type='button' onClick={handleShowAlert} id="navBtn3" className="navBarButton"/>
 			<CustomAlert type={0} isOpen={showAlert} onClose={() => setShowAlert(false)}/>
-			<button type='button' id="navBtn4" className="navBarButton"/>
 			<button type='button' id="navBtn5" className="navBarButton"/>
 		</div>
 	)
-
 }
 
 export default NavBar;
