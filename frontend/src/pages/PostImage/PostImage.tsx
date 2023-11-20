0973096e252095e945c6e35095d9cd95236df130
Nivek <kevin500nunez@gmail.com>
2023-11-20 01:36:01 -0600

    Titulo y botones

--- frontend/src/pages/PostImage/PostImage.tsx
+++ frontend/src/pages/PostImage/PostImage.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
-import PostCardImage from '../../components/layout/PostCardImage/PostCardImage';
 import './PostImage.css'
+import CustomAlert from "../../components/layout/CustomAlert/CustomAlert";
 
 
 function PostImage() {
     return (
         <div className={"post-feed-page"}>
-            <div className={"post-feed-container"}>
+            <div className={"Nav"}>
                 <h2 className="postCardTitle">Titulo</h2>
+                <div className="postCardButtonOutsideContainer">
+                    <div className="postCardButtonInerContainer">
+                        <button id="postCardSave" className="postCardActionButton" type="button"/>
+                        <button id="postCardShare" className="postCardActionButton" type="button"/>
+                        <button id="postCardBan" className="postCardActionButton" type="button"/>
+                    </div>
+                </div>
             </div>
         </div>
     )
