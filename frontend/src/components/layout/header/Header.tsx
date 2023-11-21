d33264a4c0cf725b5d7be1347059316a548b61fc
jonar12 <jona.arredondo12@gmail.com>
2023-11-20 21:16:41 -0600

    Implemented alternative Header.tsx

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,16 +1,36 @@
 import { Account } from './account';
 import "./Header.css"
+import {useLocation} from "react-router-dom";
 
 type Props = {
     isAccountVisible: boolean;
 };
 
 function Header({ isAccountVisible }: Props) {
+  const location = useLocation();
+
+  const renderHeader = () => {
+    if (location.pathname !== '/') {
+      return(
+        <>
+          <button className={"back-btn"}/>
+          <div className={"logo-serenium"}/>
+        </>
+      )
+    } else {
+      return (
+        <>
+          {isAccountVisible && <Account />}
+          <div className="logoTextSerenium" />
+        </>
+      )
+    }
+  }
+
     return (
         <header className='headerContainer'>
-            {isAccountVisible && <Account />}
-            <div className="logoTextSerenium" />
-            <button className="dropdownButtonMenu" type="button"> </button>
+          {renderHeader()}
+          <button className="dropdownButtonMenu" type="button"> </button>
         </header>
     );
 }
