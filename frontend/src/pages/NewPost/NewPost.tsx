233466ced43787d32a341473c450dce04b34ea87
jonar12 <jona.arredondo12@gmail.com>
2023-11-21 21:31:03 -0600

    New post page fully implemented

--- frontend/src/pages/NewPost/NewPost.tsx
+++ frontend/src/pages/NewPost/NewPost.tsx
@@ -9,12 +9,18 @@ const NewPost = () => {
 					<div id={"upper-section"}>
 						<input className={"new-post-input"} id={"post-title"} type={"text"} name={"post-title"} placeholder={"Title"} required={true}/>
 						<button id={"add-tags-btn"}>Add Tags</button>
-						<textarea name={"content"} placeholder={"Content (optional)"} className={"new-post-input"}/>
+						<textarea name={"content"} placeholder={"Content (optional)"} id={"content-input"} className={"new-post-input"}/>
 					</div>
 					<div id={"lower-section"}>
-						<input type={"file"} name={"image"}/>
-						<button type={"submit"}>
-							<p>Publish<span className={"add-post-svg"}></span></p>
+						<div className={"photo-input-container"}>
+							<input type={"file"} id={"photo-input"} name={"image"}/>
+							<label htmlFor={"photo-input"} className={"photo-label"}>
+								<div id={"photo-upload-icon"}></div>
+							</label>
+						</div>
+						<button id={"publish-post-btn"} type={"submit"}>
+							<p className={"publish-post-btn-text"}>Publish</p>
+							<div className={"add-post-svg"}></div>
 						</button>
 					</div>
 				</form>
