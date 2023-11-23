d0a0ddfa4d7291cef21cfe77ea6fc967f906f23b
jonar12 <jona.arredondo12@gmail.com>
2023-11-22 22:23:19 -0600

    ISSUE 49: Bug fixed and refactored SC ID and metadata variables

--- frontend/src/components/layout/PostCard/PostCard.tsx
+++ frontend/src/components/layout/PostCard/PostCard.tsx
@@ -6,6 +6,7 @@ import {useEffect, useState} from "react";
 import Tag from "../Tag/Tag";
 import Reply from "../Reply/Reply";
 import {useAlert, useApi} from "@gear-js/react-hooks";
+import {THREAD_PROGRAM_ID, THREAD_PROGRAM_METADATA} from "../../../ContractVariables";
 
 interface PostCardProps {
 	title: string;
@@ -38,19 +39,13 @@ function PostCard () {
 	const {api} = useApi();
 	const [threadState, setThreadState] = useState<ThreadState>();
 	const alert = useAlert();
-	const [showAlert, setShowAlert] = useState(false);
 	const [postExpired, setPostExpired] = useState(true);
 
-	// TODO: Get these variables from env file
-	const programIDThread = "0x4eabc8f612ac98b4c5ff37315d31dd61c5f9288038d3d041e321973c70712693";
-
-	const meta = "00010001000000000001040000000108000000000000000109000000350d40000808696f18496e69744654000004013466745f70726f6772616d5f696404011c4163746f72496400000410106773746418636f6d6d6f6e287072696d6974697665731c4163746f724964000004000801205b75383b2033325d000008000003200000000c000c0000050300100808696f30546872656164416374696f6e000110244e65775468726561640400140128496e697454687265616400000024456e64546872656164000100204164645265706c790400180118537472696e67000200244c696b655265706c7904001c01107531323800030000140808696f28496e697454687265616400001001086964180118537472696e6700012c7468726561645f74797065180118537472696e670001147469746c65180118537472696e6700011c636f6e74656e74180118537472696e6700001800000502001c0000050700200808696f2c5468726561644576656e74000110404e6577546872656164437265617465640000002c546872656164456e646564000100285265706c794164646564000200285265706c794c696b656400030000240808696f20496f54687265616400002401086964180118537472696e670001146f776e657204011c4163746f72496400012c7468726561645f74797065180118537472696e670001147469746c65180118537472696e6700011c636f6e74656e74180118537472696e6700011c7265706c69657328016c5665633c284163746f7249642c205468726561645265706c79293e0001307061727469636970616e74733401505665633c284163746f7249642c2075313238293e00011473746174653c012c546872656164537461746500014864697374726962757465645f746f6b656e731c0110753132380000280000022c002c00000408043000300808696f2c5468726561645265706c79000014011c706f73745f69641c011075313238000128706f73745f6f776e657204011c4163746f72496400011c636f6e74656e74180118537472696e6700013c6e756d6265725f6f665f6c696b65731c0110753132380001446e756d6265725f6f665f7265706f7274731c01107531323800003400000238003800000408041c003c0808696f2c5468726561645374617465000108184163746976650000001c4578706972656400010000";
-
-	const metadata = ProgramMetadata.from(meta);
+	const metadata = ProgramMetadata.from(THREAD_PROGRAM_METADATA);
 
 	const getState = () => {
 		api.programState
-			.read({ programId: programIDThread, payload: '' }, metadata)
+			.read({ programId: THREAD_PROGRAM_ID, payload: '' }, metadata)
 			.then(result => {
 				setThreadState(result.toJSON() as unknown as ThreadState);
 				alert.success('Successful state');
@@ -58,17 +53,12 @@ function PostCard () {
 			.catch(({ message }: Error) => alert.error(message));
 	};
 
+	// useEffect hook to update the thread state
 	useEffect(() => {
 		getState();
 	}, []);
 
-	const handleShowAlert = () => {
-		setShowAlert(true);
-	};
-
-	const cardPostAddButton = 'cardPostAddButton';
-	const cardPostAddButtonBlocked = postExpired ? 'cardPostAddButtonBlocked' : '';
-	const compoundClassName = `${cardPostAddButton} ${cardPostAddButtonBlocked}`;
+	const compoundClassName = `${threadState?.state === 'Expired' ? 'cardPostAddButtonBlocked' : 'cardPostAddButton'}`;
 
 	return (
 		<div className="postCard">
@@ -94,9 +84,7 @@ function PostCard () {
 						<button id="postCardShare" className="postCardActionButton" type="button"/>
 						<button id="postCardBan" className="postCardActionButton" type="button"/>
 					</div>
-					<button className={compoundClassName}  type="button" onClick={() => {
-						!postExpired && handleShowAlert()
-					}}/>
+					<button className={compoundClassName}  type="button"/>
 				</div>
 			</div>
 		</div>
--- frontend/src/pages/NewPost/NewPost.tsx
+++ frontend/src/pages/NewPost/NewPost.tsx
@@ -1,9 +1,71 @@
 import NavBar from "../../components/layout/NavBar/NavBar";
 import './NewPost.css'
 import {useState} from "react";
+import {ProgramMetadata} from "@gear-js/api";
+import {useAccount, useAlert, useApi} from "@gear-js/react-hooks";
+import {web3FromSource} from "@polkadot/extension-dapp";
+import {THREAD_PROGRAM_ID, THREAD_PROGRAM_METADATA} from "../../ContractVariables";
 
 const NewPost = () => {
+	const alert = useAlert();
 	const [selectedImg, setSelectedImg] = useState<File | null>(null);
+	const [Title, setTitle] = useState('');
+	const [Content, setContent] = useState('');
+	const { accounts, account } = useAccount();
+	const { api } = useApi();
+
+	const metadata = ProgramMetadata.from(THREAD_PROGRAM_METADATA);
+
+	const message: any = {
+		destination: THREAD_PROGRAM_ID,
+		payload: {
+			NewThread: {
+				// TODO: handle id generation
+				id: "2",
+				// TODO: Change for actual state variable of post type
+				threadType: "Question",
+				title: Title,
+				content: Content,
+			},
+		},
+		gasLimit: 2099819245,
+		value: 0,
+	};
+
+	const signer = async () => {
+		const localaccount = account?.address;
+		const isVisibleAccount = accounts.some(
+			(visibleAccount) => visibleAccount.address === localaccount
+		);
+
+		if (isVisibleAccount) {
+			// Create a message extrinsic
+			const transferExtrinsic = await api.message.send(message, metadata);
+
+			const injector = await web3FromSource(accounts[0].meta.source);
+
+			transferExtrinsic
+				.signAndSend(
+					account?.address ?? alert.error("No account"),
+					{ signer: injector.signer },
+					({ status }) => {
+						if (status.isInBlock) {
+							alert.success("Status");
+						} else {
+							console.log("transaction in Process");
+							if (status.type === "Finalized") {
+								alert.success(status.type);
+							}
+						}
+					}
+				)
+				.catch((error: any) => {
+					console.log(":( transaction failed", error);
+				});
+		} else {
+			alert.error("Account not available to sign");
+		}
+	};
 
 	const handleImgChange = (e: any) => {
 		const imgFile = e.target.files[0];
@@ -17,11 +79,15 @@ const NewPost = () => {
 	return (
 		<>
 			<div className={"new-post-container"}>
-				<form className={"form-container"}>
+				<div className={"form-container"}>
 					<div id={"upper-section"}>
-						<input className={"new-post-input"} id={"post-title"} type={"text"} name={"post-title"} placeholder={"Title"} required={true}/>
+						<input className={"new-post-input"} id={"post-title"} type={"text"} name={"post-title"} placeholder={"Title"} required={true} onChange={(e) => {
+							setTitle(e.target.value)
+						}}/>
 						<button id={"add-tags-btn"}>Add Tags</button>
-						<textarea name={"content"} placeholder={"Content (optional)"} id={"content-input"} className={"new-post-input"}/>
+						<textarea name={"content"} placeholder={"Content (optional)"} id={"content-input"} className={"new-post-input"} onChange={(e) => {
+							setContent(e.target.value)
+						}}/>
 					</div>
 					<div id={"lower-section"}>
 						<div className={"photo-input-container"}>
@@ -31,12 +97,12 @@ const NewPost = () => {
 							</label>
 							{selectedImg && <p>{selectedImg.name}</p>}
 						</div>
-						<button id={"publish-post-btn"} type={"submit"}>
+						<button id={"publish-post-btn"} onClick={signer}>
 							<p className={"publish-post-btn-text"}>Publish</p>
 							<div className={"add-post-svg"}></div>
 						</button>
 					</div>
-				</form>
+				</div>
 			</div>
 			<NavBar/>
 		</>
