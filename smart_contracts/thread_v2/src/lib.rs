525001cb26b3e3aa96a7fbf4343bd9bb6a8f7094
jonar12 <jona.arredondo12@gmail.com>
2023-11-29 01:06:48 -0600

    Implemented function for finding path winners and implemented token transaction for their rewards

--- smart_contracts/thread_v2/src/lib.rs
+++ smart_contracts/thread_v2/src/lib.rs
@@ -26,8 +26,8 @@ struct Thread {
 impl Thread {
 
     async fn tokens_transfer_reward(&mut self, amount_tokens: u128, dest: ActorId) {
-        let address_ft = addresft_state_mut();           
-        let payload = FTAction::Transfer {from: exec::program_id(), to: dest ,amount: amount_tokens};
+        let address_ft = addresft_state_mut();
+        let payload = FTAction::Transfer {from: exec::program_id(), to: dest,amount: amount_tokens};
         let _ = msg::send(address_ft.ft_program_id, payload, 0);
     }
 
@@ -40,7 +40,7 @@ impl Thread {
         current_state.distributed_tokens += amount_tokens;
     }
 
-    fn find_winner(&mut self) -> Option<&ActorId> {
+    fn find_winner_actor_id(&mut self) -> Option<&ActorId> {
         let current_state = thread_state_mut();
         let mut max_likes = 0;
         let mut actor_id_with_most_likes: Option<&ActorId> = None;
@@ -54,6 +54,72 @@ impl Thread {
 
         actor_id_with_most_likes
     }
+
+    fn find_winner_post_id(&mut self) -> Option<&String> {
+        let current_state = thread_state_mut();
+        let mut max_likes = 0;
+        let mut post_id_winner: Option<&String> = None;
+
+        for (actor_id, reply) in &current_state.replies {
+            if reply.number_of_likes > max_likes {
+                max_likes = reply.number_of_likes;
+                post_id_winner = Some(&reply.post_id);
+            }
+        }
+        post_id_winner
+    }
+
+    fn find_actor_id_by_post_id(&mut self, target_post_id: String) -> Option<&ActorId> {
+        for (actor_id, reply) in self.replies.iter() {
+            if *reply.post_id == *target_post_id {
+                return Some(actor_id)
+            }
+        }
+        None
+    }
+
+    fn find_path_to_winner(&mut self, original_post_id: &String) -> Vec<ActorId> {
+        let mut path_winners: Vec<ActorId> = Vec::new();
+
+        // immediately add actor id of original post
+        let original_post_actor_id = self.find_actor_id_by_post_id(original_post_id.clone())
+            .expect("Actor ID not obtained correctly");
+        path_winners.push(*original_post_actor_id);
+
+        // find id of winner post
+        let winner_id = self.find_winner_post_id()
+            .expect("ActorID of winner not correctly found");
+
+        // initially search for the winner
+        let mut target_id = winner_id.clone();
+
+        // Collect graph_rep values into a separate variable to end immutable borrow earlier
+        let values: Vec<_> = self.graph_rep.values().cloned().collect();
+
+        while target_id != *original_post_id {
+            let mut found_reply = false;
+            for adj_list in &values {
+                for reply_id in adj_list {
+                    // if reply id is the id of the target,
+                    if *reply_id == target_id {
+                        // find the actor id
+                        let actor_id = self.find_actor_id_by_post_id(reply_id.clone())
+                            .expect("ActorID not adequately found");
+                        // append to the vector we will return.
+                        path_winners.push(actor_id.clone());
+                        target_id = reply_id.clone();
+                        found_reply = true;
+                        break;
+                    }
+                }
+                if found_reply {
+                    break;
+                }
+            }
+        }
+        path_winners
+    }
+
 }
 
 static mut THREAD: Option<Thread> = None;
@@ -116,17 +182,28 @@ async fn main() {
 
             // send delayed message to expire thread
             let payload = ThreadAction::EndThread;
-            let _ = msg::send_delayed(exec::program_id(), payload, 0, 5).expect("Delayed expiration msg was not successfully sent");
+            let delay = 60;
+            let _end_thread_msg = msg::send_delayed(exec::program_id(), payload, 0, delay).expect("Delayed expiration msg was not successfully sent");
 
             // transfer a token
-            // new_thread.tokens_transfer_pay(1).await;
+            new_thread.tokens_transfer_pay(1).await;
         }
 
         ThreadAction::EndThread => {
             let thread = thread_state_mut();
             thread.state = ThreadState::Expired;
-            // let &winner = thread.find_winner().expect("Winner not found");
-            // thread.tokens_transfer_reward(1, winner).await;
+            let &winner = thread.find_winner_actor_id().expect("Winner not found");
+
+            let distributed_tokens = thread.distributed_tokens;
+            // calculate amount of tokens to distribute
+            let tokens_for_abs_winner = (distributed_tokens.clone() as f64 * 0.4) as u128;
+
+            thread.tokens_transfer_reward(tokens_for_abs_winner, winner).await;
+            let path_winners = thread.find_path_to_winner(&thread.id.clone());
+            let tokens_for_each_path_winner = (distributed_tokens.clone() as f64 * 0.4 / path_winners.len() as f64) as u128;
+            for actor in path_winners {
+                thread.tokens_transfer_reward(tokens_for_each_path_winner, actor).await;
+            }
         }
 
         ThreadAction::AddReply(content, reply_id, post_id) => {
