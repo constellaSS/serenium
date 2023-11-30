fe5f5c3fe0fb36a19bd773b5d482f2854aa37113
Nivek <kevin500nunez@gmail.com>
2023-11-29 20:25:33 -0600

    Added Login screen

---
+++ frontend/src/pages/Profile/Profile.tsx
@@ -0,0 +1,22 @@
+import './Profile.css'
+
+function Profile() {
+    return (
+        <div className={"full-screen"}>
+            <div className={"contentWrapper"}>
+                <div className={"user"}>
+                    <div className={"user-image"}/>
+                    <h2 className={"user-name"}>Lou</h2>
+                </div>
+                <div className="Polkadot">
+                </div>
+                <div className="logout-button">
+                    <button id="logout" className="logout-button1" type="button"/>
+                    <h2 className={"user-name"}>Log out</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
 import './home.css'
 import NavBar from "../../components/layout/NavBar/NavBar";
 import PostFeed from "../PostFeed/PostFeed";
+import Profile from "../Profile/Profile";
 
 function Home() {
   return (
     <div className='homeScreen'>
-      <PostFeed/>
+      <Profile/>
       <NavBar/>
     </div>
   )
