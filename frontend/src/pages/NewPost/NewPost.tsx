570579102c14689f40b6226e6b306c7a7aced81e
Lou <Lou@constella.tech>
2023-11-30 06:13:17 -0600

    Added fixes to input screen,

--- frontend/src/pages/NewPost/NewPost.tsx
+++ frontend/src/pages/NewPost/NewPost.tsx
@@ -134,14 +134,26 @@ const NewPost = ({isReply}: NewPostProps) => {
 		};
 		handlePhotoUrlChange();
 	}, [photoUrl]);
+	let isQuestion = ()=>{if(ThreadType ==="Question"){return true}else {return false}};
+	const dynamicBackground = {
 
+
+		backgroundImage: isQuestion() ? "./newPostChallengeBG.svg" : "./newPostQuestionBG.svg",
+		// Add more styles as needed
+	};
 	return (
 		<>
-			<div className={"new-post-container"}>
+			<div className={"new-post-container"} style={dynamicBackground}>
 				<div className={"form-container"}>
 					<div id={"upper-section"}>
-						<h2 className={"new-post-heading"}>New {isReply? 'Reply' : 'Post'}</h2>
-						{!isReply && <TypeDropdown threadType={ThreadType} setThreadType={setThreadType}/>}
+
+						<div className={"new-post-upper-row"}>
+
+							<h2 className={"new-post-heading"}>New {isReply? 'Reply' : 'Post'}</h2>
+							{!isReply && <TypeDropdown threadType={ThreadType} setThreadType={setThreadType}/>}
+						</div>
+
+
 						<input className={"new-post-input"} id={"post-title"} type={"text"} name={"post-title"} placeholder={"Title"} value={Title} required={true} onChange={(e) => {
 							setTitle(e.target.value)
 						}}/>
