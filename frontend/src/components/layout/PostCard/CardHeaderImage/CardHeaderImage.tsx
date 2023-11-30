11863c0df058e2bcd5794beef9a54515cf410ca4
Nivek <kevin500nunez@gmail.com>
2023-11-29 23:22:45 -0600

    Established max height and width

--- frontend/src/components/layout/PostCard/CardHeaderImage/CardHeaderImage.tsx
+++ frontend/src/components/layout/PostCard/CardHeaderImage/CardHeaderImage.tsx
@@ -5,10 +5,12 @@ interface CardHeaderImageProps {
 
 const CardHeaderImage = ({imgUrl}: CardHeaderImageProps) => {
 	return (
-		<div id={"card-header-image-container"} onClick={() => {
+		<div className={"image-first-container"} id={"card-header-image-container"} onClick={() => {
 			window.location.href = '/full-screen-post'
 		}}>
-			<img alt={"post-picture"} src={imgUrl} id={"post-photo"}/>
+			<div className={"image-second-container"}>
+				<img alt={"post-picture"} src={imgUrl} id={"post-photo"}/>
+			</div>
 			<div id={"type-section"} ></div>
 		</div>
 	)
