10d061c9703bece404fbe0e059a5981fa6e6f193
Nivek <kevin500nunez@gmail.com>
2023-12-12 16:51:07 -0600

    tag module

--- frontend/src/components/layout/Tag/Tag.tsx
+++ frontend/src/components/layout/Tag/Tag.tsx
@@ -1,12 +1,12 @@
-import './Tag.css'
+import styles from './Tag.module.css'
 interface TagProps {
 	name: string
 }
 const Tag = ({name}: TagProps) => {
 	return (
 		<>
-			<div className={"tag-container"}>
-				<p className={"tag-text"}>{`#${name}`}</p>
+			<div className={styles.tagContainer}>
+				<p className={styles.tagText}>{`#${name}`}</p>
 			</div>
 		</>
 	)
