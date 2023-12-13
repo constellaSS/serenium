22836f38921d1deb84306e42662204717586c394
Nivek <kevin500nunez@gmail.com>
2023-12-12 18:34:00 -0600

    Added user info

--- frontend/src/pages/PostFeed/PostFeed.tsx
+++ frontend/src/pages/PostFeed/PostFeed.tsx
@@ -1,14 +1,14 @@
 import PostCard from "../../components/layout/PostCard/PostCard";
-import './PostFeed.css'
+import styles from './PostFeed.module.css'
 import PostWithReplies from "../PostWithReplies/PostWithReplies";
 import PostImage from "../PostImage/PostImage";
 import NewPost from "../NewPost/NewPost";
 
 const PostFeed = () => {
 	return (
-		<div className={"post-feed-page"}>
-			<div className={"post-feed-container"}>
-				<PostCard/>
+		<div className={styles.postFeedPage}>
+			<div className={styles.postFeedContainer}>
+				<PostImage/>
 			</div>
 		</div>
 	)
--- frontend/src/pages/PostImage/PostImage.tsx
+++ frontend/src/pages/PostImage/PostImage.tsx
@@ -1,9 +1,30 @@
 import styles from './PostImage.module.css'
+import {extractLast10Digits} from "../../utils/extract_from_string";
+import {useAccount} from "@gear-js/react-hooks";
+import {useState} from "react";
 
 function PostImage() {
+	const { account, accounts } = useAccount();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<div className={styles.fullScreenPostPage}>
 			<div className={styles.contentWrapper}>
+				<div className={styles.userInfo}>
+					<div className={styles.polkadotLogo}/>
+					<div className={styles.accountNameContainer}>
+						<h2 className={styles.userName}>{account?.meta.name as string}</h2>
+						<p className={styles.actorId}>...{extractLast10Digits(account?.decodedAddress as string)}</p>
+					</div>
+				</div>
 				<div className={styles.textPost}>
 					<h2 className={styles.postCardTile}>Titulo</h2>
 				</div>
